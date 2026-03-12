f44468f177cf97129edcfacb87e5b10f8943b783
Al Amin <alamin.dev01@gmail.com>
2026-03-13 03:04:49 +0600

    added some code

--- closer.js
+++ closer.js
@@ -10,19 +10,35 @@ result(); // This will call the innerFunction and print "Hello from the inner fu
 // innerFunction() is not accessible here, but we can call it through the result variable
 // console.log('in the outer function:', result);
 
-function counter() {
+function counter(owner) {
     let count = 0;
     function increment() {
         count = count + 1; // This will increment the count variable by 1 each time increment is called
-        console.log('Value of count',count);
+        console.log('Value of count', owner, count);
     }
     return increment;
 }
+const rahimCounter = counter('Rohim');
+rahimCounter()
+rahimCounter()
+rahimCounter()
+rahimCounter()
+rahimCounter()
+rahimCounter()
+const karimCounter = counter('Karim');
+karimCounter()
+karimCounter()
+karimCounter()
+karimCounter()
+karimCounter()
 
-const count1 = counter();
-count1(); // This will print "Value of count 0" because count is initialized to 0 and increment is called without modifying it.
-count1(); // This will print "Value of count 1" because count is initialized to 0 and increment is called, modifying it.
-count1(); // This will print "Value of count 2" because count is initialized to 0 and increment is called, modifying it.
-count1(); // This will print "Value of count 3" because count is initialized to 0 and increment is called, modifying it.
+rahimCounter()  
+rahimCounter()
 
-console.log(count1)
+// const count1 = counter();
+// count1(); // This will print "Value of count 0" because count is initialized to 0 and increment is called without modifying it.
+// count1(); // This will print "Value of count 1" because count is initialized to 0 and increment is called, modifying it.
+// count1(); // This will print "Value of count 2" because count is initialized to 0 and increment is called, modifying it.
+// count1(); // This will print "Value of count 3" because count is initialized to 0 and increment is called, modifying it.
+// count1(); // This will print "Value of count 4" because count is initialized to 0 and increment is called, modifying it.
+// console.log(rahimCounter)
